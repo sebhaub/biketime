4e9bb01c1a7f6dc421ec304cca50bc966e910fdd
Sebastian Haubner <sebastian.haubner@fau.de>
2015-01-26 13:52:20 +0100

    more comments

--- src/org/biketime/bike/suspension/Suspension.java
+++ src/org/biketime/bike/suspension/Suspension.java
@@ -13,30 +13,40 @@ public interface Suspension {
 	/***
 	 * 
 	 * @return the type-object of this implementation
+     * @methodtype query
+     * @methodproperties primitive hook
 	 */
 	public BikePart getType();
 
 	/***
 	 * 
 	 * @return a human readable String of this implementations suspension
+     * @methodtype conversion
+     * @methodproperties primitive hook
 	 */
     public String asString();
 
     /***
      * 
      * @return true if this implementations have a suspension
+     * @methodtype query
+     * @methodproperties primitive hook
      */
     public boolean hasSuspension();
 
     /***
      * 
      * @returns the {@link org.biketime.bike.suspension.SuspensionType}
+     * @methodtype query
+     * @methodproperties primitive hook
      */
     public SuspensionType getSuspensionType();
 
     /***
      * Sets the {@link org.biketime.bike.suspension.SuspensionType} to type @see java.lang.Object.Enum#valueOf(String name)
-     * @param type The name of the SuspensionType to set 
+     * @param type The name of the SuspensionType to set
+     * @methodtype set
+     * @methodproperties hook
      */
     public Suspension setSuspensionType(String type) throws BikePartInitializationException;
 
@@ -44,6 +54,8 @@ public interface Suspension {
      * Sets the travel of a {@link org.biketime.bike.suspension.SuspensionType}
      * @param value the travel in mm
      * @param type the {@link org.biketime.bike.suspension.SuspensionType}
+     * @methodtype set
+     * @methodproperties hook
      */
     public Suspension setSuspensionTravel(int value, SuspensionType type) throws BikePartInitializationException;
 
@@ -51,9 +63,10 @@ public interface Suspension {
      * Returns the SuspensionTravel for a given type
      * @param type the {@link org.biketime.bike.suspension.SuspensionType}
      * @return
+     * @methodtype query
+     * @methodproperties primitive hook
      */
     public String getSuspensionTravel(SuspensionType type);
-
 
     public boolean equals(Object obj);
 
